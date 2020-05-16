2f7b259e928dca090938e07937fff3311caa7280
Darryl Leong <darryleong95@gmail.com>
2020-05-16 22:12:54 +0800

    env vars

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import * as firebase from 'firebase';
 var config = {
-    apiKey: "AIzaSyDToPGriOjvguAGOwf2Yxtkkeh5oR1UlMI",
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: "razer-hackathon.firebaseapp.com",
     databaseURL: "https://razer-hackathon.firebaseio.com",
     projectId: "razer-hackathon",
     storageBucket: "razer-hackathon.appspot.com",
-    messagingSenderId: "890279692437",
-    appId: "1:890279692437:web:79626a48ee61ec3803739c",
-    measurementId: "G-NHG5X99LLS"
+    messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 firebase.initializeApp(config);
 
